399cc06948a4f06728f1007773c0e75c7594f5b8
Elias Estay <=elias.estay@gmail.com>
2016-11-15 15:34:20 -0300

    correccion para descarga de archivo

--- openassessment/xblock/static/js/src/lms/oa_response_extra.js
+++ openassessment/xblock/static/js/src/lms/oa_response_extra.js
@@ -54,11 +54,6 @@ setTimeout('temp_css()', 5000);
 setInterval('timer_button()',3000);
 
 
-$(".submission--image").click(function(){
- var href = $('.submission--image').attr('href');
- var href = href.substring(0,href.length - 1);
- var pdfext = '.pdf';
- var href = href + pdfext;
- console.log(href);
-});
+
+
 
