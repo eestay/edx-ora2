281217a84d53b6cba78aa56098ac7a5087548500
eestay <eestay@uchile.cl>
2017-01-23 16:44:46 -0300

    correccion en version

--- openassessment/xblock/static/js/src/lms/oa_response_extra.js
+++ openassessment/xblock/static/js/src/lms/oa_response_extra.js
@@ -6,32 +6,35 @@ function temp_tmce() {
     plugins: ['advlist autolink lists link anchor'],
     menubar: false,
     language_url : '/tiny_es.js',
-    content_css: '//www.tinymce.com/css/codepen.min.css',        
+    content_css: '//www.tinymce.com/css/codepen.min.css',
     init_instance_callback: function (editor) {
         var saved_text = tinymce.DOM.getOuterHTML(submission__answer__part__text__1);
         tinymce.activeEditor.setContent( saved_text , {format: 'raw'});
         tinymce.activeEditor.dom.remove('submission__answer__part__text__1', true);
-        editor.on('focus', function (e) { 
-            var real_text = tinymce.activeEditor.getContent({format: 'raw'}); 
+        editor.on('init', function (e) {
+            var real_text = tinymce.activeEditor.getContent({format: 'raw'});
                 tinymce.DOM.setHTML('submission__answer__part__text__1', real_text);
-                console.log('focus:', e.target.nodeName);
-                });  
-        editor.on('change', function (e) { 
-            var real_text = tinymce.activeEditor.getContent({format: 'raw'}); 
+                });
+        editor.on('focus', function (e) {
+            var real_text = tinymce.activeEditor.getContent({format: 'raw'});
+                tinymce.DOM.setHTML('submission__answer__part__text__1', real_text);
+                });
+        editor.on('change', function (e) {
+            var real_text = tinymce.activeEditor.getContent({format: 'raw'});
                             tinymce.DOM.setHTML('submission__answer__part__text__1', real_text);
                             });
-        editor.on('keyup', function (e) { 
-            var real_text = tinymce.activeEditor.getContent({format: 'raw'}); 
+        editor.on('keyup', function (e) {
+            var real_text = tinymce.activeEditor.getContent({format: 'raw'});
                             tinymce.DOM.setHTML('submission__answer__part__text__1', real_text);
                             });
-        editor.on('drop', function (e) { 
-            var real_text = tinymce.activeEditor.getContent({format: 'raw'}); 
+        editor.on('drop', function (e) {
+            var real_text = tinymce.activeEditor.getContent({format: 'raw'});
                             tinymce.DOM.setHTML('submission__answer__part__text__1', real_text);
                             });
-        editor.on('paste', function (e) { 
-            var real_text = tinymce.activeEditor.getContent({format: 'raw'});  
+        editor.on('paste', function (e) {
+            var real_text = tinymce.activeEditor.getContent({format: 'raw'});
                             tinymce.DOM.setHTML('submission__answer__part__text__1', real_text);
-                            });                                                                                   
+                            });
                         }
                     });
     }
@@ -61,7 +64,3 @@ setTimeout('temp_tmce_or()', 5000);
 setTimeout('temp_css()', 5000);
 setInterval('timer_button()',3000);
 setInterval('temp_tmce_or()',2000);
-
-
-
-
