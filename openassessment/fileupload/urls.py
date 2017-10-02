57b35c1c6173c491070bc8c81964216c064a305c
Elias Estay <eestay@uchile.cl>
2017-10-02 12:29:17 -0300

    correcciones en url y cache

--- openassessment/fileupload/urls.py
+++ openassessment/fileupload/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('openassessment.fileupload.views_filesystem',
-    url(r'^(?P<key>.+)/$', 'filesystem_storage', name='openassessment-filesystem-storage'),
+    url(r'^submissions_attachments(?P<key>.+)/$', 'filesystem_storage', name='openassessment-filesystem-storage'),
 )
