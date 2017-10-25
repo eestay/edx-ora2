c22488bce8a4174576df3de52ae0dc3605ade1ec
Elias Estay <elias.estay@gmail.com>
2017-10-25 16:49:26 -0300

    correcciones nueva

--- openassessment/xblock/submission_mixin.py
+++ openassessment/xblock/submission_mixin.py
@@ -174,12 +174,14 @@ class SubmissionMixin(object):
         student_sub_dict = prepare_submission_for_serialization(student_sub_data)
 
         if self.allow_file_upload:
-            upload_dir = self._get_student_item_key().split('/',1)
+            #upload_dir = self._get_student_item_key().split('/',1)
             logging.info('\n\n\n dir\n')
             logging.info(self._get_student_item_key())
-            logging.info(upload_dir)
-            logging.info("/edx/var/ora2/upload/edxuploads/submissions_attachments/"+upload_dir[0]+'/'+upload_dir[1])
-            if(os.path.isdir("/edx/var/ora2/upload/edxuploads/"+upload_dir[0]+'/'+upload_dir[1])):
+            #logging.info(upload_dir)
+            #logging.info("/edx/var/ora2/upload/edxuploads/submissions_attachments/"+upload_dir[0])
+            current_dir = self._get_student_item_key().replace("..", "").strip("/ ")
+            logging.info(current_dir)
+            if(os.path.isdir("/edx/var/ora2/upload/edxuploads/"+current_dir):
                 logging.info('exits dir ')
                 student_sub_dict['file_key'] = self._get_student_item_key()
             else:
